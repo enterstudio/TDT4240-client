d3ccbc980b09e7f173e7bbd148dbde52f93d67ad
Sigurd <groneng93@gmail.com>
2017-03-09 13:01:47 +0100

    Add fragment from android template

--- app/src/main/java/com/gruppe16/tdt4240_client/MainActivity.java
+++ app/src/main/java/com/gruppe16/tdt4240_client/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gruppe16.tdt4240_client;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gruppe16.tdt4240_client.fragments.MenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initializeStartFragment(){
+        MenuFragment fragment = MenuFragment.newInstance("hey", "yo");
+        FragmentManager fm = getSupportFragmentManager();
+        fm
+                .beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
     }
 }
