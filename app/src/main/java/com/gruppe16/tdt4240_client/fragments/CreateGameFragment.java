8b9e086f9486455358a217e653c3964de9201a5e
daphneleebeek <daphneleebeek@gmail.com>
2017-03-10 11:13:22 +0100

    Created getters and setters for dynamic numbers in createGameFragment. Created a exit fragment and view with a setter for the player who wins

--- app/src/main/java/com/gruppe16/tdt4240_client/fragments/CreateGameFragment.java
+++ app/src/main/java/com/gruppe16/tdt4240_client/fragments/CreateGameFragment.java
@@ -10,22 +10,39 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.gruppe16.tdt4240_client.FragmentChanger;
 import com.gruppe16.tdt4240_client.R;
 
 public class CreateGameFragment extends Fragment {
 
     private Button startGameButton;
-    private TextView gamePin;
-    private TextView playersCount;
+    private TextView gamePinTextView;
+    private TextView playersCountTextView;
+    private int gamePin;
+    private int playersCount;
+
 
     public CreateGameFragment() {
         // Required empty public constructor
+        setPin();
+        setPlayers();
     }
 
     public static CreateGameFragment newInstance() {
         CreateGameFragment fragment = new CreateGameFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
         return fragment;
     }
+
+    public int getPin(){ return gamePin; }
+
+    public void setPin(){ gamePin = 12345; }
+
+    public int getPlayers(){ return playersCount; }
+
+    public void setPlayers(){ playersCount = 3; }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,19 +54,19 @@ public class CreateGameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_create_game, container, false);
+        gamePinTextView = (TextView) rootView.findViewById(R.id.gamePinTextView);
+        playersCountTextView = (TextView) rootView.findViewById(R.id.playersCountTextView);
         startGameButton = (Button) rootView.findViewById(R.id.startGameButton);
-        gamePin = (TextView) rootView.findViewById(R.id.gamePin);
-        playersCount = (TextView) rootView.findViewById(R.id.playersCount);
 
+        gamePinTextView.setText("PIN: " + getPin());
+        playersCountTextView.setText("Player: " + getPlayers());
 
         startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //FragmentChanger.goToDrawView(getActivity());
+            public void onClick(View v) {FragmentChanger.goToDrawView(getActivity());
             }
         });
 
         return rootView;
-
     }
 }
