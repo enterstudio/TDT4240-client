72a1d6fee6f90485d16cb2cae978a551255a3d41
Sigurd <groneng93@gmail.com>
2017-04-20 15:12:06 +0200

    Dont enable player to go on if data is not ready

--- app/src/main/java/com/gruppe16/tdt4240_client/fragments/FinishedFragment.java
+++ app/src/main/java/com/gruppe16/tdt4240_client/fragments/FinishedFragment.java
@@ -3,20 +3,52 @@ package com.gruppe16.tdt4240_client.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.android.volley.Response;
+import com.gruppe16.tdt4240_client.GameState;
+import com.gruppe16.tdt4240_client.NetworkAbstraction;
 import com.gruppe16.tdt4240_client.R;
 import com.gruppe16.tdt4240_client.FragmentChanger;
 import com.gruppe16.tdt4240_client.interfaces.OnGoToView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class FinishedFragment extends Fragment {
 
+    private static String TAG = "FinishedFragment";
+
     private Button startSlideshowButton;
     private OnGoToView onGoToView;
+    private Timer pollTimer;
+
+    private Response.Listener<JSONObject> listener = new Response.Listener<JSONObject>() {
+        @Override
+        public void onResponse(JSONObject response) {
+            try{
+                int round = (Integer) response.get("round");
+                int numberOfPlayers = GameState.getInstance().getNumberOfPlayers();
+                if(round == (numberOfPlayers * 2) - 1 ){
+                    startSlideshowButton.setClickable(true);
+                    startSlideshowButton.setAlpha(1f);
+                    pollTimer.cancel();
+                }
+            }
+            catch (JSONException e){
+                Log.d(TAG, e.toString());
+                return;
+            }
+        }
+    };
 
     // Required empty public constructor
     public FinishedFragment() {}
@@ -26,6 +58,15 @@ public class FinishedFragment extends Fragment {
         return fragment;
     }
 
+    public void pollForAllDataReceived(){
+        pollTimer = new Timer();
+        pollTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                NetworkAbstraction.getInstance(getContext()).getGame(listener);
+            }
+        }, 0, 1000);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +78,8 @@ public class FinishedFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_finishedfragment, container, false);
         startSlideshowButton = (Button) rootView.findViewById(R.id.startSlideshowButton);
-
+        startSlideshowButton.setClickable(false);
+        startSlideshowButton.setAlpha(0.5f);
         startSlideshowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
